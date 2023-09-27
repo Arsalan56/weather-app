21d7464de6c623100ae40d1e53ab91a42bcd0155
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-09-26 20:11:17 -0400

    Create basic design of header
    The icon and search bar are added to the icon. For now, the button that
    allows information to be changed from c/f is not implemented, but has
    been reserved a spot.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
+import './icon.png';
+
 import GetWeather from './getWeather';
 
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    GetWeather(document.querySelector('input').value);
-});
+// document.querySelector('form').addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     GetWeather(document.querySelector('input').value);
+// });
