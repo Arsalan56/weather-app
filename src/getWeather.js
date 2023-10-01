c10cd55f19d15bcbd09c91bff8521854cb761459
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-10-01 19:50:17 -0400

    Create an error handler for getting location
    If the user inputted location is invalid, then an error message is
    displayed under the search bar.

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,15 +1,29 @@
-export default function GetWeather(i) {
-    fetch(
-        `https://api.weatherapi.com/v1/current.json?key=4377390ce908483ab8510059232409&q=${i}`,
-        {
-            mode: 'cors',
-        }
-    )
-        .then((response) => response.json())
-        .then((resp) => {
-            console.log(resp);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+export default async function GetWeather(i) {
+    console.log(
+        await fetch(
+            `https://api.weatherapi.com/v1/search.json?key=4377390ce908483ab8510059232409&q=${i}`,
+            {
+                mode: 'cors',
+            }
+        )
+            .then((response) => response.json())
+            .then(async (resp) => {
+                console.log(resp);
+                if (resp.length === 0) {
+                    throw new Error('Location not found');
+                } else {
+                    return fetch(
+                        `https://api.weatherapi.com/v1/current.json?key=4377390ce908483ab8510059232409&q=${i}`,
+                        {
+                            mode: 'cors',
+                        }
+                    )
+                        .then((w) => w.json())
+                        .then((weather) => weather);
+                }
+            })
+            .catch((err) => {
+                document.querySelector('.error-msg').textContent = err;
+            })
+    );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './icon.png';
 
 import GetWeather from './getWeather';
 
-// document.querySelector('form').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     GetWeather(document.querySelector('input').value);
-// });
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    GetWeather(document.querySelector('input').value);
+});
