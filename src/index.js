98b1a5290b326829c6635d924999926422909cb6
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-10-06 19:02:11 -0400

    Add some styling to hourly forecast

--- src/index.js
+++ src/index.js
@@ -6,5 +6,11 @@ import GetWeather from './getWeather';
 document.querySelector('form').addEventListener('submit', async (e) => {
     e.preventDefault();
     const weather = await GetWeather(document.querySelector('input').value);
-    console.log(weather);
+    document
+        .querySelector('.hourlyCast>div>img')
+        .setAttribute(
+            'src',
+            weather.forecast.forecastday[0].hour[8].condition.icon
+        );
+    // console.log(weather.forecast.forecastday[0].hour[8].condition.icon);
 });
