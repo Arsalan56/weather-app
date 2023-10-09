16adc77e57b1e28e198b7ef337522e43063b8bad
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-10-08 20:45:09 -0400

    Finish styling page
    The daily cast changes flex direction based on screen size. If the
    screen gets too small, the daily cast displays information vertically.
    If it gets big again, then the pieces are horizontally spread.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import './style.css';
 import './icon.png';
 
 import GetWeather from './getWeather';
+import Check from './sizeCheck';
 
-document.querySelector('form').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const weather = await GetWeather(document.querySelector('input').value);
-    document
-        .querySelector('.hourlyCast>div>img')
-        .setAttribute(
-            'src',
-            weather.forecast.forecastday[0].hour[8].condition.icon
-        );
-    // console.log(weather.forecast.forecastday[0].hour[8].condition.icon);
-});
+(() => {
+    document.querySelector('form').addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const weather = await GetWeather(document.querySelector('input').value);
+        document
+            .querySelector('.hourlyCast>div>img')
+            .setAttribute(
+                'src',
+                weather.forecast.forecastday[0].hour[8].condition.icon
+            );
+        // console.log(weather.forecast.forecastday[0].hour[8].condition.icon);
+    });
 
-// eslint-disable-next-line no-restricted-globals
-addEventListener('resize', () => {
-    console.log('o');
-});
+    // eslint-disable-next-line no-restricted-globals
+    // window.onresize = Check();
+    window.onresize = Check;
+})();
--- src/sizeCheck.js
+++ src/sizeCheck.js
@@ -1,4 +1,18 @@
+const dailyCont = document.querySelector('.dailyCont');
+const highLow = document.querySelectorAll('.dailyCont > div > div');
 export default function Check() {
-    if (window.innerWidth > 600) {
+    if (window.innerWidth < 600) {
+        if (!dailyCont.hasAttribute('style')) {
+            dailyCont.style['flex-direction'] = 'column';
+            highLow.forEach((i) => {
+                // eslint-disable-next-line no-param-reassign
+                i.style['flex-direction'] = 'row';
+            });
+        }
+    } else {
+        dailyCont.removeAttribute('style');
+        highLow.forEach((i) => {
+            i.removeAttribute('style');
+        });
     }
 }
