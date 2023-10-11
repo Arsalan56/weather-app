0d8cca6a41351642efa9224f6fa27ca02984c43b
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-10-11 17:45:17 -0400

    Fix header display on smaller screens
    The header originally had unaligned divs since the "weather app" text
    would wrap and affect the lining. The text is now smaller so wrapping
    wouldn't be necessary.

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import generalFill from './generalFill';
         // console.log(weather);
         hourlyFill(weather, true);
         generalFill(weather, true);
+        forecastFill(weather, true);
     });
 
     // eslint-disable-next-line no-restricted-globals
